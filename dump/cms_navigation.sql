c70bac07560e59d4d6ae64accb002dd62ccc8b62
ShpiliakYurii <shpilak.yura@gmail.com>
2018-05-16 18:14:33 +0300

    database dump

---
+++ dump/cms_navigation.sql
@@ -0,0 +1,5 @@
+INSERT INTO cms.navigation (id, caption, page) VALUES (1, 'Home', 1);
+INSERT INTO cms.navigation (id, caption, page) VALUES (2, 'Blog', 2);
+INSERT INTO cms.navigation (id, caption, page) VALUES (3, 'About', 3);
+INSERT INTO cms.navigation (id, caption, page) VALUES (4, 'Portfolio', 4);
+INSERT INTO cms.navigation (id, caption, page) VALUES (5, 'Contact', 5);
---
+++ dump/cms_page.sql
@@ -0,0 +1,5 @@
+INSERT INTO cms.page (id, name, template, title) VALUES (1, 'home', 'view/content/body.jsp', 'Simpler');
+INSERT INTO cms.page (id, name, template, title) VALUES (2, 'blog', 'view/content/blog.jsp', 'Blog');
+INSERT INTO cms.page (id, name, template, title) VALUES (3, 'about', 'view/content/about.jsp', 'About');
+INSERT INTO cms.page (id, name, template, title) VALUES (4, 'portfolio', 'view/content/portfolio.jsp', 'Portfolio');
+INSERT INTO cms.page (id, name, template, title) VALUES (5, 'contact', 'view/content/contact.jsp', 'Contact');
