511bed1e3ba07cb47477b998dd6fcefe7b17dd00
ShpiliakYurii <shpilak.yura@gmail.com>
2018-06-15 14:35:26 +0300

    add some test methods

--- src/main/java/com/cms/solution/DataController.java
+++ src/main/java/com/cms/solution/DataController.java
@@ -1,5 +1,9 @@
 package com.cms.solution;
 
+import com.cms.solution.entity.Page;
+import com.cms.solution.service.i.PageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -17,11 +21,30 @@ public class DataController {
     @Autowired
     private DataSource datasource;
 
+    @Autowired
+    private PageService pageService;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataController.class);
+
     @GetMapping("/data/init")
     public ResponseEntity<Void> intiData() throws SQLException {
         Resource resource = new ClassPathResource("insert_data.sql");
         ScriptUtils.executeSqlScript(datasource.getConnection(), resource);
         return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+
+    @GetMapping("/data/home")
+    public ResponseEntity<Object> getHomePage() {
+        Page page = pageService.getByName(PageConstants.HOME_PAGE);
+        LOGGER.debug("page: {}", page);
+        return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/data/test")
+    public ResponseEntity<Object> getTest() {
+        return new ResponseEntity<>("{page: home}", HttpStatus.OK);
     }
 
 }
